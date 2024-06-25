038422c7c6aa171886dab7136bb8f228ecfd55fb
hiso8155 <hiso8155@gmail.com>
2024-06-25 09:49:48 +0000

    Trying to add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'KurodaKJ\'s Portfolio',
+  title: "KurodaKJ's Portfolio",
   description: 'Created with Next.js',
 }
 
@@ -15,6 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
